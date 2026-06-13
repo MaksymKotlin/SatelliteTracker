4afec411f6b22eb29cc911a53f05f9be0bb182cd
MaksymKotlin <opsec303@gmail.com>
2026-06-13 20:46:13 +0200

    Fix: Timer overlap during TLE fetch

--- SatelliteTracker2D/ViewModels/MainWindowViewModel.cs
+++ SatelliteTracker2D/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Timers;
+
 using CommunityToolkit.Mvvm.ComponentModel;
+
 using SatelliteTracker2D.Services;
 
 namespace SatelliteTracker2D.ViewModels;
@@ -12,14 +14,11 @@ public partial class MainWindowViewModel : ObservableObject
     private readonly IssPredictor _issPredictor = new();
     private readonly Timer _timer = new(1000);
 
-    [ObservableProperty]
-    private double _latitude;
+    [ObservableProperty] private double _latitude;
 
-    [ObservableProperty]
-    private double _longitude;
+    [ObservableProperty] private double _longitude;
 
-    [ObservableProperty]
-    private double _speed;
+    [ObservableProperty] private double _speed;
 
     public MainWindowViewModel()
     {
@@ -39,10 +38,21 @@ public partial class MainWindowViewModel : ObservableObject
 
     private async void OnTimerElapsed(object? sender, ElapsedEventArgs e)
     {
-        string[] tleLines = await _tleService.GetTleLinesAsync();
-        _issPredictor.LoadTle(tleLines);
+        _timer.Stop();
+        try
+        {
+            string[] tleLines = await _tleService.GetTleLinesAsync();
+            _issPredictor.LoadTle(tleLines);
 
-        (Latitude, Longitude, Speed) = _issPredictor.GetCurrentPosition();
+            (Latitude, Longitude, Speed) = _issPredictor.GetCurrentPosition();
+        }
+        catch
+        {
+        }
+        finally
+        {
+            _timer.Start();
+        }
 
         Console.WriteLine($"ISS → Lat: {Latitude:F4}°  Lon: {Longitude:F4}°");
     }
